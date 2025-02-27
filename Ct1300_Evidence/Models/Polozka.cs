29d962e05615606170b3af4b3e91f081f6c59f7d
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2025-02-27 14:33:50 +0100

    Přidejte soubory projektu.

---
+++ Ct1300_Evidence/Models/Polozka.cs
@@ -0,0 +1,16 @@
+﻿namespace Ct1300_Evidence.Models
+{
+	public class Polozka
+	{
+		public Polozka()
+		{
+			Datum = DateOnly.FromDateTime(DateTime.Now);
+		}
+
+		public DateOnly Datum { get; set; }
+		public double Naklady { get; set; }
+		public double Vynosy { get; set; }
+		public string Popis { get; set; } = "";
+		public double Zisk => Vynosy - Naklady;
+	}
+}
---
+++ Ct1300_Evidence/Pages/EvidenceZisku.razor.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace Ct1300_Evidence.Pages
+{
+	public partial class EvidenceZisku
+	{
+		#region Vlastnosti
+		/// <summary>
+		/// Seznam položek evidence zisku.
+		/// </summary>
+		public List<Models.Polozka> Polozky { get; set; } = new List<Models.Polozka>();
+
+		public Models.Polozka Polozka { get; set; } = new Models.Polozka();
+
+		#endregion
+	}
+}
